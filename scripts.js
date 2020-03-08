3a88b11f2e5573beb5c43682d62a1ea444eb7bb9
zazou2418 <zazou2418@gmail.com>
2020-03-08 17:01:49 +0100

    Ajout du code pour la Multiplication et la division

--- scripts.js
+++ scripts.js
@@ -12,5 +12,27 @@ $('#calculSous').click(() => {
     let chiffreSous2 = $('#chiffreSous2').val();
     let calculSous = Number(chiffreSous1) - Number(chiffreSous2); 
     $('#resultSous').html(calculSous);
-}) 
-   
+    
+})
+
+$('#calculMulti').click(() => {
+    let chiffreMulti1 = $('#chiffreMulti1').val();
+    let chiffreMulti2 = $('#chiffreMulti2').val();
+    let calculMulti = Number(chiffreMulti1) * Number(chiffreMulti2); 
+    $('#resultMulti').html(calculMulti);
+})
+
+$('#calculDivi').click(() => {
+    let chiffreDivi1 = $('#chiffreDivi1').val();
+    let chiffreDivi2 = $('#chiffreDivi2').val();
+    let calculDivi = Number(chiffreDivi1) / Number(chiffreDivi2); 
+    $('#resultDivi').html(calculDivi);
+})
+
+
+
+
+
+
+
+

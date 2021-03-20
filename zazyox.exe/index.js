321f7a83c0e6e4de78b75065ce678610a414ec60
anthonywispelaereweb <contact@wispelaere.fr>
2021-03-20 18:52:40 +0100

    Add Commands

--- zazyox.exe/index.js
+++ zazyox.exe/index.js
@@ -10,11 +10,14 @@ Client.on("ready", () =>{
 })
  
 Client.on("message", message =>{
-    if (message.content === 'Hello') message.reply('Hello');
-    if (message.content.includes('Comment')) message.reply('t\'es curieux :truck: !');
-    if (message.content.startsWith('Louis')) message.reply('Le beau Gosse :smile: !');
+    if (message.content === 'Hey') message.reply('Bonsoir');
+    if (message.content.includes('Briend')) message.reply('BRIEND ? Toujours sous un bureau :blond_haired_woman:');
+    if (message.content.startsWith('Louis')) message.reply('Celui qui m\'a dévelppé ?');
+    if (message.content.includes('Lacote')) message.reply('LACOTE ? C\'est un BG au FC Lambersart :wink:');
     if (message.content === '0' ) message.reply('C\'est un chiffre :smile: !');
+    if (message.content.includes('Zazyox.exe')) message.reply('On m\'a appelé ? ');
 });
+
 
 // https://www.smashingmagazine.com/2021/02/building-discord-bot-discordjs/
 Client.login(config.token);
